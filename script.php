9e625ab9a07bef36fe21d6a8ce4765037afe301c
RomZOne <hervier.romuald@gmail.com>
2014-11-28 00:40:15 +0100

    Create script.php

---
+++ script.php
@@ -0,0 +1,8 @@
+<?php
+
+setlocale (LC_TIME, 'fr_FR.utf8','fra'); 
+echo (strftime("Bonjour nous sommes le <b>%A %d %B</b> <br><br>"));
+
+echo 'la suite du script sera disponible dès que Mangoo sera finalisé';
+
+?>

f16c6cbea91000e6678f2cff0b527e5edb5e5ec4
RomZOne <hervier.romuald@gmail.com>
2014-11-28 00:41:17 +0100

    Update script.php

--- script.php
+++ script.php
@@ -3,6 +3,6 @@
 setlocale (LC_TIME, 'fr_FR.utf8','fra'); 
 echo (strftime("Bonjour nous sommes le <b>%A %d %B</b> <br><br>"));
 
-echo 'la suite du script sera disponible dès que Mangoo sera finalisé';
+echo "la suite du script sera disponible dès que Mangoo sera finalisé";
 
 ?>
